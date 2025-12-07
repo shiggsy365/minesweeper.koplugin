c719e250d3df665328835f6946e6d4fc0f8ef56b
shiggsy365 <shiggsy365@gmail.com>
2025-12-07 01:37:03 +0000

    Create _meta.lua for Minesweeper game
    Add metadata for Minesweeper game including name, fullname, and description.

---
+++ _meta.lua
@@ -0,0 +1,6 @@
+local _ = require("gettext")
+return {
+    name = "minesweeper",
+    fullname = _("Minesweeper"),
+    description = _([[A classic Minesweeper game for KOReader. Tap to reveal cells, long-press to place flags.]]),
+}
